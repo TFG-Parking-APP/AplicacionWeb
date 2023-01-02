0c488fea3b5f4ca7d7b1870dbeba03da9b8619cf
RempalagoMcWilson <juanmemos08@gmail.com>
2023-01-02 11:59:51 +0100

    primera version de pago

---
+++ public/scripts/pay.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const myModalAlternative = new bootstrap.Modal("#myModal");
+
+$("#payBtn").on("click", function() {
+    console.log(myModalAlternative);
+    $("#spn").css('display', '');
+    console.log($("#carId").val());
+    $.ajax({
+        method: "GET",
+        url: "/datosPago",
+        data: {
+            carId: $("#carId").val()
+        },
+        // En caso de éxito, mostrar el resultado
+        // en el documento HTML
+        success: function(data, textStatus, jqXHR) {
+            console.log(textStatus);
+            $("#resultado").text("El resultado es " + data.resultado);
+            $("#spn").css('display', 'none');
+            $("#infoPago").text(data.resultado);
+            $("#infoTiempo").text(data.tiempoEstacionado);
+            myModalAlternative.show();
+        },
+        // En caso de error, mostrar el error producido
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert("Se ha producido un error: " + errorThrown);
+        }
+    });
+});
--- routes/carRoute.js
+++ routes/carRoute.js
@@ -10,40 +10,50 @@ let carService = new CarService();
 
 app.post("/newCar",
     multerFactory.single('image'),
-    function (request, response) {
-        
-    let usuario = request.session.usuario;
-    if (usuario){
-        carService.newCar(request, response, request.file, usuario.id);
-    }
-    else{
-        response.redirect("login");
-    }
-});
+    function(request, response) {
+
+        let usuario = request.session.usuario;
+        if (usuario) {
+            carService.newCar(request, response, request.file, usuario.id);
+        } else {
+            response.redirect("login");
+        }
+    });
 
 app.get("/imagenCoche/:id", (request, response) =>
     carService.getImage(request, response)
 );
 
-app.get("/car/:id", (request, response) =>{
+app.get("/car/:id", (request, response) => {
     let usuario = request.session.usuario;
-    if (usuario){
-        carService.getCarById(request.params.id , (car) => {
+    if (usuario) {
+        carService.getCarById(request.params.id, (car) => {
             response.status(200);
-            response.render("car.ejs", {usuario, coche : car[0] });
+            response.render("car.ejs", { usuario, coche: car[0] });
         })
-        
-    }
-    else{
+
+    } else {
         response.redirect("login");
     }
 });
 
-app.post("/carMove/:plate",function (request, response) { 
+app.post("/carMove/:plate", function(request, response) {
     response.status(200);
     console.log(request.params.plate);
     console.log(request.body);
     response.end;
+});
+
+app.get("/datosPago", function(request, response) {
+    response.status(200);
+    let numero = request.query.carId;
+    console.log(numero);
+    response.json({ resultado: '20 €', tiempoEstacionado: '3 horas' });
+});
+
+app.post("/pagar", function(request, response) {
+    response.status(200);
+    response.redirect("login");
 });
 
 module.exports = app;
